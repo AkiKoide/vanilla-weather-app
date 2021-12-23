f2cfd4ec126ab3d046fe7ad1184b9bde3bec18de
AkiKoide <akikoide924@gmail.com>
2021-12-23 17:53:45 +0100

    Showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#cityTempreture");
+  let cityElement = document.querySelector("#city");
+  let discriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  discriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "3399623ab1e4ceda0e2cf5eebdf36ab9";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
